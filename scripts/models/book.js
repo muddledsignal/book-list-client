40047a4cc02f5d087eeb224fe3fb38c3ac70f6b8
Chris L Chapman <ChristopherLChapman42@gmail.com>
2018-06-20 17:12:15 -0700

    update from class code review

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -3,12 +3,20 @@ var app = app || {};
 
 (function(module){ // start of second IIFE from user/dev stories 
   
+  // function errorCallback(err) { //inits error page if an error
+
+  // } // errorCallback
+  
   function Book (rawDataObj) { // TODO?: refactor to .map? 
     Object.keys(rawDataObj).forEach(key => {
       this[key] = rawDataObj[key]
     }, this);
   }
   Book.all = []; 
+  Book.prototype.toHtml = function() {
+    return app.render('book-list-template', this);
+  }
+
   Book.loadAll = rows => {
     rows.sort((a, b) => {
       if (a.title.toUpperCase() < b.title.toUpperCase()) { return -1 };
@@ -18,16 +26,13 @@ var app = app || {};
     Book.all = rows.map(bookObj => new Book(bookObj)); 
   }
 
-  Book.prototype.toHtml = function() {
-    return app.render('book-list-template', this);
-  }
-Book.fetchAll = callback => {
+  Book.fetchAll = callback => {
    $.get('/api/v1/books')
      .then(results => {
        Book.loadAll(results);
        callback();
      }).catch(err => console.log(err) ); 
- };
+  };
   
   module.Book = Book; 
 })(app); // end of second IIFE
