af8e7035596b338d9a382d56537cc705bd8102c3
Chris L Chapman <ChristopherLChapman42@gmail.com>
2018-06-22 13:57:53 -0700

    form showing for add a book when called from console

---
+++ scripts/views/add-view.js
@@ -0,0 +1,16 @@
+'use strict';
+var app = app || {};
+
+(function(module) { // start IIFE
+  // if (!module.bookView) {
+  //   var bookView = {};
+  // }
+
+  module.bookView.initAddBookPage = () => {
+    app.showOnly('.add-book');
+    
+    // we need an listner for the form
+    // we need a path to INSERT a record in DB 
+  }; // end bookView.initAddBookPage
+
+})(app); // end IIFE
